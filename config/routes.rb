e4c621bffcc5586423f3540d1c604b895b09bfe0
jjccc <jjcanocortes@outlook.com>
2018-02-13 12:37:59 +0100

    Update pictures

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Juanjosecanocortes::Application.routes.draw do
   
-  get "prediciendo" => "home#prediciendo", :as => "prediciendo"
-  get "metirium" => "home#metirium", :as => "metirium"
-  get "thealarmfactory" => "home#thealarmfactory", :as => "thealarmfactory"
+  # get "prediciendo" => "home#prediciendo", :as => "prediciendo"
+  # get "metirium" => "home#metirium", :as => "metirium"
+  # get "thealarmfactory" => "home#thealarmfactory", :as => "thealarmfactory"
   get "finderforperiscope" => "home#finderforperiscope", :as => "finderforperiscope"
-  get "turismovital" => "home#turismovital", :as => "turismovital"
+  # get "turismovital" => "home#turismovital", :as => "turismovital"
+  
+  get "cem" => "home#cem", :as => "cem"
+  get "eventoxperienzia" => "home#eventoxperienzia", :as => "eventoxperienzia"
+  get "quiz" => "home#quiz", :as => "quiz"
   
   resources :home, :only => [:index]
   resources :contacts, :only => [:index, :create]
